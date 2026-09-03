c8556e76c956f26fc58457ba21d60225168a7be8
nani <mikhilrampati2006@gamil.com>
2026-09-03 13:44:32 +0530

    Phase 1

---
+++ models/listing.js
@@ -0,0 +1,32 @@
+
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema= new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:String,
+    image:{
+        filename:
+         {
+        type: String,
+        default: "listingimage",
+        },
+        url:
+        {
+        type: String,
+        default: "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?auto=format&fit=crop&w=800&q=60",
+        set: (v) => v === "" 
+        ? "https://images.unsplash.com/photo-1499793983690-e29da59ef1c2?auto=format&fit=crop&w=800&q=60" 
+        : v,
+        }                                       
+    },
+    price:Number,
+    location:String,
+    country:String,
+});
+
+const Listing=mongoose.model("Listing",listingSchema);
+module.exports=Listing;
